1b7b384b3beb3949feea1cec53a76c96c195c2b8
rd124p7 <rd124p7@yahoo.com>
2019-02-09 15:07:12 -0500

    2 Major Changes Read Description...
    Removed old Unit Testing VS project due to improper creation of the project. A new project for unit testing has been created.

---
+++ DB/DBTest/DB_Unit_Testing/DB_Unit_Testing/DBConnection_Test.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DB_Unit_Testing
+{
+    [TestClass]
+    public class DBConnection_Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
