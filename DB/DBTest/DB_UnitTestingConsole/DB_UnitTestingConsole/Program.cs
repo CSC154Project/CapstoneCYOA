90369bf219ea4c236347b2b2a197031e5f6958ac
rd124p7 <rd124p7@yahoo.com>
2019-02-09 16:17:31 -0500

    Added some Yummy files

--- DB/DBTest/DB_UnitTestingConsole/DB_UnitTestingConsole/Program.cs
+++ DB/DBTest/DB_UnitTestingConsole/DB_UnitTestingConsole/Program.cs
@@ -10,6 +10,47 @@ namespace DB_UnitTestingConsole
     {
         static void Main(string[] args)
         {
+            // Create a connection to the database
+            CapstoneDBDataContext CapstoneDB = new CapstoneDBDataContext(Properties.Settings.Default.capstoneConnectionString);
+            int flag = 0;
+
+            // Check to make sure the database exists, 
+            // If it does exist. Set flag to 1,
+            // if it is still 0 the queries won't run
+            if (CapstoneDB.DatabaseExists())
+            {
+                Console.WriteLine("The DB Exsists");
+                flag = 1;
+            }
+            else
+            {
+                Console.WriteLine("The DB Does not Exsist");
+                flag = 0;
+            }
+
+            if (flag == 1)
+            {
+                try
+                {
+                    // Run the query and store the results in the questionQuery variable.
+                    // There is an issue with querying the "Question" table it will throw an exception
+                    var questionQuery = CapstoneDB.ExecuteQuery<Encounter>(@"SELECT * FROM Encounter");
+
+                    // Loop through the questionQuery list and print every single item
+                    Console.WriteLine("EncID \t" + "EncTypeID");
+                    foreach (Encounter encounter in questionQuery)
+                        Console.WriteLine(encounter.EncounterID + "\t" + encounter.EncounterTypeID);
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }   
+            }
+
+            Console.ReadKey();
+
+
+
         }
     }
 }
