13f35b077fa819950b068d4fe81b363d7b21ec60
rd124p7 <rd124p7@yahoo.com>
2019-02-15 17:41:25 -0500

    Created DBTestConn, DBConnException

---
+++ DB/DBTest/DB_UnitTestingConsole/DB_UnitTestingConsole/DBConnections/DBTestConn.cs
@@ -0,0 +1,49 @@
+﻿/*
+    Class Name: DBTestConn
+    Purpose:
+        NOTE: Use only for Unit Testing
+
+        Easliy connect, query and close the data base. Makes connecting, querying, and closing the database
+
+    Inherits: None       
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DB_UnitTestingConsole.DBConnections
+{
+    class DBTestConn
+    {
+        // private instance variables
+        private string strConnectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=capstone;Integrated Security=True";
+        private SqlConnection conn;
+
+
+        /*
+            Constructor Name: DBTestConn
+            Description:
+                Create a connection to the database and open the connection, 
+                if it can't be open throw a DBConnException
+
+            Params: None
+        */
+        public DBTestConn()
+        {
+            try
+            {
+                conn = new SqlConnection(strConnectionString);
+                conn.Open();
+            }
+            catch (SqlException ex)
+            {
+                throw new DBConnException("Unable to connect to Database.", ex);
+            }
+                
+        }
+    }
+}
