f426ea805a0a01343b6d34b3bfb6c9dad81e59e6
rd124p7 <rd124p7@yahoo.com>
2019-02-25 15:08:24 -0500

    Re-Wrote DBConn.cs Class

---
+++ DB/DBTest/DB_UnitTestingConsole/DB_UnitTestingConsole/DBConn/DBConn.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DB_UnitTestingConsole.DBConn
+{
+    public class Encounter
+    {
+        public int EncounterID { get; set; }
+        public int EncounterTypeID { get; set; }
+
+        public override string ToString()
+        {
+            return EncounterID.ToString() + "\t" + EncounterTypeID.ToString();
+        }
+    }
+
+    public class Questions
+    {
+        public int EncID { get; set; }
+        public int ID { get; set; }
+        public string Text { get; set; }
+
+        public override string ToString()
+        {
+            return EncID.ToString() + "\t" + ID.ToString() + "\t" + Text;
+        }
+    }
+
+    public class Choices
+    {
+        public int EncID { get; set; }
+        public int ID { get; set; }
+        public int QuestionID { get; set; }
+        public string Text { get; set; }
+        public int NextEID { get; set; }
+
+        public override string ToString()
+        {
+            return EncID.ToString() + "\t" + ID.ToString() + "\t" + QuestionID.ToString() 
+                + "\t" + Text + "\t" + NextEID.ToString();
+        }
+    }
+
+    public class DBConn
+    {
+        private SqlConnection conn;
+
+        public DBConn()
+        {
+            // Connect to the db
+            string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=capstone;Integrated Security=True";
+
+            try
+            {
+                conn = new SqlConnection(connectionString);
+                conn.Open();
+            }
+            catch(SqlException Ex)
+            {
+                Console.WriteLine(Ex.ToString());
+            }
+        }
+
+        public List<object> RunQuery(string query, object classType)
+        {
+            List<object> lstQueryResults = new List<object>();
+            SqlCommand cmd = new SqlCommand(query, conn);
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            if (classType is Encounter)
+            {
+                while(reader.Read())
+                {
+                    Encounter enc = new Encounter();
+                    enc.EncounterID = (int)reader[0];
+                    enc.EncounterTypeID = (int)reader[1];
+                    
+                    lstQueryResults.Add(enc);
+                }
+            }
+            else if (classType is Questions)
+            {
+                while (reader.Read())
+                {
+                    Questions question = new Questions();
+                    question.EncID = (int)reader[0];
+                    question.ID = (int)reader[1];
+                    question.Text = (string)reader[2];
+
+                    lstQueryResults.Add(question);
+                }
+            }
+            else if (classType is Choices)
+            {
+                while (reader.Read())
+                {
+                    Choices choice = new Choices();
+                    choice.EncID = (int)reader[0];
+                    choice.ID = (int)reader[1];
+                    choice.QuestionID = (int)reader[2];
+                    choice.Text = (string)reader[3];
+                    choice.NextEID = (int)reader[4];
+
+                    lstQueryResults.Add(choice);
+                }
+            }
+
+            conn.Close();
+
+            return lstQueryResults;
+        }
+    }
+}
--- DB/DBTest/DB_UnitTestingConsole/DB_UnitTestingConsole/Program.cs
+++ DB/DBTest/DB_UnitTestingConsole/DB_UnitTestingConsole/Program.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
-using DB_UnitTestingConsole.DBConnections;
 
 namespace DB_UnitTestingConsole
 {
@@ -11,140 +10,144 @@ namespace DB_UnitTestingConsole
         static void Main(string[] args)
         {
             /*
-            // Connect to the db
-            string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=capstone;Integrated Security=True";
-            SqlConnection connectMe = new SqlConnection(connectionString);
+             // Connect to the db
+             string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=capstone;Integrated Security=True";
+             SqlConnection connectMe = new SqlConnection(connectionString);
 
-            List<string> result = new List<string>();
-            string query;
-            SqlDataAdapter da;
-            DataTable dt;
+             List<string> result = new List<string>();
+             string query;
+             SqlDataAdapter da;
+             DataTable dt;
 
-            int colNum = 0;
+             int colNum = 0;
 
-            // Open the db
-            connectMe.Open();
+             // Open the db
+             connectMe.Open();
 
-            if (connectMe.State != ConnectionState.Open)
-            {
-                Console.WriteLine("Unable to open the database.");
-            }
-            else
-            {
-                Console.WriteLine("Successfully opened the database...\nNow checking tables...");
+             if (connectMe.State != ConnectionState.Open)
+             {
+                 Console.WriteLine("Unable to open the database.");
+             }
+             else
+             {
+                 Console.WriteLine("Successfully opened the database...\nNow checking tables...");
 
-                // Check contents of each table in db
-                // Encounter Table
-                query = "SELECT * FROM ENCOUNTER";
-                SqlCommand cmd = new SqlCommand(query, connectMe);
-                da = new SqlDataAdapter(cmd);
-                dt = new DataTable();
-                da.Fill(dt);
+                 // Check contents of each table in db
+                 // Encounter Table
+                 query = "SELECT * FROM ENCOUNTER";
+                 SqlCommand cmd = new SqlCommand(query, connectMe);
+                 da = new SqlDataAdapter(cmd);
+                 dt = new DataTable();
+                 da.Fill(dt);
 
-                Console.WriteLine("\nEncounter\n----------");
-                foreach (DataRow row in dt.Rows)
-                {
-                    foreach (DataColumn column in dt.Columns)
-                    {
-                        Console.Write("{0, -9}", row[column]);
-                        colNum++;
+                 Console.WriteLine("\nEncounter\n----------");
+                 foreach (DataRow row in dt.Rows)
+                 {
+                     foreach (DataColumn column in dt.Columns)
+                     {
+                         Console.Write("{0, -9}", row[column]);
+                         colNum++;
 
-                        if (colNum == dt.Columns.Count)
-                        {
-                            Console.WriteLine();
-                            colNum = 0;
-                        }
-                    }
-                }
+                         if (colNum == dt.Columns.Count)
+                         {
+                             Console.WriteLine();
+                             colNum = 0;
+                         }
+                     }
+                 }
 
-                Console.WriteLine("\nEncounter contains " + dt.Rows.Count + " rows and " + dt.Columns.Count + " columns.");
+                 Console.WriteLine("\nEncounter contains " + dt.Rows.Count + " rows and " + dt.Columns.Count + " columns.");
 
-                // EncounterType Table
-                query = "SELECT * FROM ENCOUNTERTYPE";
-                cmd = new SqlCommand(query, connectMe);
-                da = new SqlDataAdapter(cmd);
-                dt = new DataTable();
-                da.Fill(dt);
+                 // EncounterType Table
+                 query = "SELECT * FROM ENCOUNTERTYPE";
+                 cmd = new SqlCommand(query, connectMe);
+                 da = new SqlDataAdapter(cmd);
+                 dt = new DataTable();
+                 da.Fill(dt);
 
-                Console.WriteLine("\n\nEncounterType\n--------------");
-                foreach (DataRow row in dt.Rows)
-                {
-                    foreach (DataColumn column in dt.Columns)
-                    {
-                        Console.Write("{0, -9}", row[column]);
-                        colNum++;
+                 Console.WriteLine("\n\nEncounterType\n--------------");
+                 foreach (DataRow row in dt.Rows)
+                 {
+                     foreach (DataColumn column in dt.Columns)
+                     {
+                         Console.Write("{0, -9}", row[column]);
+                         colNum++;
 
-                        if (colNum == dt.Columns.Count)
-                        {
-                            Console.WriteLine();
-                            colNum = 0;
-                        }
-                    }
-                }
+                         if (colNum == dt.Columns.Count)
+                         {
+                             Console.WriteLine();
+                             colNum = 0;
+                         }
+                     }
+                 }
 
-                Console.WriteLine("\nEncounter Type contains " + dt.Rows.Count + " rows and " + dt.Columns.Count + " columns.");
+                 Console.WriteLine("\nEncounter Type contains " + dt.Rows.Count + " rows and " + dt.Columns.Count + " columns.");
 
-                // Questions Table
-                query = "SELECT * FROM QUESTIONS";
-                cmd = new SqlCommand(query, connectMe);
-                da = new SqlDataAdapter(cmd);
-                dt = new DataTable();
-                da.Fill(dt);
+                 // Questions Table
+                 query = "SELECT * FROM QUESTIONS";
+                 cmd = new SqlCommand(query, connectMe);
+                 da = new SqlDataAdapter(cmd);
+                 dt = new DataTable();
+                 da.Fill(dt);
 
-                Console.WriteLine("\n\nQuestions\n----------");
-                foreach (DataRow row in dt.Rows)
-                {
-                    foreach (DataColumn column in dt.Columns)
-                    {
-                        Console.Write("{0, -9}", row[column]);
-                        colNum++;
+                 Console.WriteLine("\n\nQuestions\n----------");
+                 foreach (DataRow row in dt.Rows)
+                 {
+                     foreach (DataColumn column in dt.Columns)
+                     {
+                         Console.Write("{0, -9}", row[column]);
+                         colNum++;
 
-                        if (colNum == dt.Columns.Count)
-                        {
-                            Console.WriteLine();
-                            colNum = 0;
-                        }
-                    }
-                }
+                         if (colNum == dt.Columns.Count)
+                         {
+                             Console.WriteLine();
+                             colNum = 0;
+                         }
+                     }
+                 }
 
-                Console.WriteLine("\nQuestions contains " + dt.Rows.Count + " rows and " + dt.Columns.Count + " columns.");
+                 Console.WriteLine("\nQuestions contains " + dt.Rows.Count + " rows and " + dt.Columns.Count + " columns.");
 
-                // Choices Table
-                query = "SELECT * FROM CHOICES";
-                cmd = new SqlCommand(query, connectMe);
-                da = new SqlDataAdapter(cmd);
-                dt = new DataTable();
-                da.Fill(dt);
+                 // Choices Table
+                 query = "SELECT * FROM CHOICES";
+                 cmd = new SqlCommand(query, connectMe);
+                 da = new SqlDataAdapter(cmd);
+                 dt = new DataTable();
+                 da.Fill(dt);
 
-                Console.WriteLine("\n\nChoices\n--------");
-                foreach (DataRow row in dt.Rows)
-                {
-                    foreach (DataColumn column in dt.Columns)
-                    {
-                        Console.Write("{0, -15}", row[column]);
-                        colNum++;
+                 Console.WriteLine("\n\nChoices\n--------");
+                 foreach (DataRow row in dt.Rows)
+                 {
+                     foreach (DataColumn column in dt.Columns)
+                     {
+                         Console.Write("{0, -15}", row[column]);
+                         colNum++;
 
-                        if (colNum == dt.Columns.Count)
-                        {
-                            Console.WriteLine();
-                            colNum = 0;
-                        }
-                    }
-                }
+                         if (colNum == dt.Columns.Count)
+                         {
+                             Console.WriteLine();
+                             colNum = 0;
+                         }
+                     }
+                 }
 
-                Console.WriteLine("\nChoices contains " + dt.Rows.Count + " rows and " + dt.Columns.Count + " columns.");
-            }
+                 Console.WriteLine("\nChoices contains " + dt.Rows.Count + " rows and " + dt.Columns.Count + " columns.");
+             }
 
-            connectMe.Close();
-            */
+             connectMe.Close();
+             */
 
-            DBTestConn conn = new DBTestConn();
-            conn.RunQuery(@"SELECT * FROM Encounter");
-            
-         
 
+
+            DBConn.DBConn conn = new DBConn.DBConn();
+            List<object> lstResult = conn.RunQuery("SELECT * FROM Choices", new DBConn.Choices());
+
+            for (int i = 0; i < lstResult.Count; i++)
+                Console.WriteLine(lstResult[i].ToString());
             Console.WriteLine("\n\n\n\nPress any key to exit.");
             Console.ReadKey();
+
         }
+
     }
 }
