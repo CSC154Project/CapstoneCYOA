c22bff70a77e2bace2a24fb40f9b353132f3d058
rd124p7 <rd124p7@yahoo.com>
2019-02-25 19:36:15 -0500

    DB_UnitTesting Project Created Within The Solution

---
+++ DB/DBTest/DB_UnitTestingConsole/DB_UnitTesting/DBConnectionTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DB_UnitTesting
+{
+    [TestClass]
+    public class DBConnectionTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
